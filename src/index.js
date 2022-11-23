ae6f83a9222e1db796cf7015841cdfa632ca5a8d
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2022-11-23 09:05:35 +0100

    started js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+
+const form = document.getElementById('search');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userInput = document.querySelector('search-input');
+    getWeather(userInput.value);
+    userInput.value = '';
+});
