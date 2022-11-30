1ffae9bb201f2c423a66d5f0cb3650ee6dfc8b32
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2022-11-30 11:47:39 +0100

    updated index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import './styles.css';
 
 let lastLocation;
+
+function getCurrentPosition(options) {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+}
+
+const getWeather = async (location) => {
+    showLoadingAnimation();
+    try {
+        const currentWeather = await getCurrentWeather(
+            location,
+            unitsToggle.className
+        );
+        displayCurrentWeather(currentWeather);
+        const forecastWeather = await getForecastWeather(
+            currentWeather,
+            unitsToggle.className
+        );
+        displayForecastWeather(forecastWeather);
+    } catch (err) {
+        showError(err);
+    }
+};
+
 const search = (e) => {
     e.preventDefault();
     const location = searchForm.text.value;
@@ -18,3 +43,36 @@ const toggleUnits = () => {
         getWeather(location);
     }
 };
+
+const getWeatherByCoords = async (coords) => {
+    showLoadingAnimation();
+    try {
+        const [currentWeather, forecastWeather] = await Promise.all([
+            getCurrentWeatherByCoords(coords, unitsToggle.className),
+            getForecastWeather(coords, unitsToggle.className),
+        ]);
+        displayCurrentWeather(currentWeather);
+        displayForecastWeather(forecastWeather);
+    } catch (err) {
+        showError(err);
+    }
+};
+
+const getWeatherAtStart = async () => {
+    const options = {
+        timeout: 5000,
+        maximumAge: 0,
+    };
+    showLoadingAnimation();
+    try {
+        const position = await getCurrentPosition(options);
+        getWeatherByCoords(position.coords);
+    } catch (err) {
+        getWeather('Agadir');
+    }
+};
+
+searchForm.addEventListener('submit', search);
+unitsToggle.addEventListener('click', toggleUnits);
+
+getWeatherAtStart();
