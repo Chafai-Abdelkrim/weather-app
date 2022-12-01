88e10d8ce62615c3f25f654920d6ce99783be8f9
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2022-12-01 11:43:31 +0100

    added function for hourly and daily dom creation

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -21,3 +21,47 @@ const createElement = (tag, attributes, ...children) => {
     });
     return element;
 };
+
+const createWeatherIcon = ({ icon, description }) => {
+    const weatherIcon = createElement('img', {
+        src: `https://openweathermap.org/img/wn/${icon}@4x.png`,
+        alt: capitalize(description),
+        title: capitalize(description),
+    });
+
+    return weatherIcon;
+};
+
+const createHourlyCard = (weather) => {
+    const hour = createElement('div', { class: 'hour' }, weather.hour);
+    const icon = createWeatherIcon(weather);
+    const temp = createElement(
+        'div',
+        { class: 'temp' },
+        `${weather.temperature}°`
+    );
+    const container = createElement('div', { class: 'container' }, icon, temp);
+    const card = createElement('div', { class: 'card' }, hour, container);
+
+    return card;
+};
+
+const createDailyCard = (weather) => {
+    const day = createElement('div', { class: 'day' }, weather.day);
+    const icon = createWeatherIcon(weather);
+    const maxTemp = createElement(
+        'div',
+        { class: 'max-temp' },
+        `${weather.maxTemperature}°`
+    );
+    const minTemp = createElement(
+        'div',
+        { class: 'min-temp' },
+        `${weather.minTemperature}°`
+    );
+    const temp = createElement('div', { class: 'temp' }, maxTemp, minTemp);
+    const container = createElement('div', { class: 'container' }, icon, temp);
+    const card = createElement('div', { class: 'card' }, day, container);
+
+    return card;
+};
